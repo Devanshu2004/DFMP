37c4a989c0cd43c0a284338d4643eb48fd072473
Devanshu Joshi <92581265+Devanshu2004@users.noreply.github.com>
2024-07-10 04:22:12 +0530

    Update data.py

--- data.py
+++ data.py
@@ -4,31 +4,47 @@ import time
 # Set up the serial connection (replace 'COM3' with your serial port)
 ser = serial.Serial('/dev/cu.usbserial-10', 9600, timeout=1)
 
+ding_counter = 0
 data_counter = 0
 value = 0
 
 # Open a file to save the readings
-with open('ultrasonic_readings.txt', 'w') as file:
+with open('ideal.txt', 'w') as file:
     while True:
         if ser.in_waiting > 0:
             # Read the data from the serial port
             line = float(ser.readline().decode('utf-8').strip())
             
-            if data_counter == 19:
-                value = value / 20
-                
-                # Print the data to the console (optional)
-                print(value)
+            if ding_counter == 45:
+                # Print output to terminal
+                print("9999.00 \n")
                 
                 # Write the data to the file
-                file.write(value + '\n')
+                file.write('9999.00 \n')
                 
-                value = 0
-                data_counter = 0
+                # Reset the ding counter
+                ding_counter = 0
             else:
-                value = value + line
-                data_counter += 1
-                print(value)
+                if data_counter == 19:
+                    # Take average of the 20 readings taken to remove error
+                    value = value / 20
+                    value = round(value, 2)
+                    
+                    # Print the data to the console (optional)
+                    print(value)
+                    
+                    # Write the data to the file
+                    file.write(str(value) + '\n')
+
+                    # Increment ding counter
+                    ding_counter += 1
+                    
+                    # Reset the counters
+                    value = 0
+                    data_counter = 0
+                else:
+                    value = value + line
+                    data_counter += 1
 
         # Small delay to avoid high CPU usage
         time.sleep(0.1)
